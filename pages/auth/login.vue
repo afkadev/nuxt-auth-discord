ec539f74fc8350567e8c8a4f0409c18571b093a8
afkadev <44179602+afkadev@users.noreply.github.com>
2023-07-23 10:03:14 +0700

    custom

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,12 +1,15 @@
 <script setup>
 const { signIn, session, status, cookies } = useAuth()
 //definePageMeta({ auth: true })
+const signinDiscord = () => {
+    location.href = 'https://discord.com/api/oauth2/authorize?client_id=1128715170890395719&redirect_uri=https%3A%2F%2Fauthjs-nuxt.unpai.red%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email'
+}
 </script>
 
 <template>
     <div>
         <div>
-            <button @click="signIn(`discord`)">
+            <button @click="signinDiscord()">
                 Signin
             </button> | 
         </div>
